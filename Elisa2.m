ca3e072a0ab3fe0154dd5792df39eb2c81fdc79c
ElisaAppolloni <elisaappolloni@gmil.com>
2015-07-24 19:55:02 +0200

    removed comments

--- Elisa2.m
+++ Elisa2.m
@@ -1,16 +1,11 @@
 
 clear all; close all; clc;
 
-% this plot shows nothing special
-
-% Some documentation here.. bla bla
 r = normrnd(1,3,[1 100]);
 plot(r)
 
-% Another plot seems good here
+
 r = normrnd(1,1,[1 140]);
 plot(r)
 
-% added a comment
 
-% added another comment
